27bb321468bd34e5f5800ac0e655d43c3aeeb099
Timothy Tsui <timiscoding@gmail.com>
2016-02-18 23:45:56 +1100

    added about, skills, brief. fullpage.js, smooth scrolling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-$(document).foundation();
+var data = ['Javascript', 'Ruby', 'HTML', 'CSS'];
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      responsiveHeight: 570,
+      anchors: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th']
+    });
+
+    // console.log(s.node().getBBox());
+    // .each(function(d, i){
+    //   console.log(d, i, this);//, this.getBBox());
+    // });
+
+  // data.forEach(function(d){
+  //   // console.log('d', d);
+  //   $('.chart').append( $('div') );
+  // });
+});
