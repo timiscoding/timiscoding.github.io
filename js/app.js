b3b45b0a6d64561618326b902fe52dcd515321fc
Timothy Tsui <timiscoding@gmail.com>
2016-02-19 19:58:11 +1100

    Colour scheme. Responsive menu. Pic and CV

--- js/app.js
+++ js/app.js
@@ -3,9 +3,29 @@ var data = ['Javascript', 'Ruby', 'HTML', 'CSS'];
 $(document).ready(function() {
     $('#fullpage').fullpage({
       responsiveHeight: 570,
-      anchors: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th']
+      anchors: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th'],
+      menu: '#menu'
     });
 
+
+    $('#menu li:first-child').on('click', function(){
+      console.log('clicked name');
+      $('#menu li').toggleClass('showMenuItem');
+    });
+
+    $('#menu li:not(:first)').on('click', function(e){
+      if (e.button === 0) {
+        $('#menu li:not(:first)').removeClass('showMenuItem');
+      }
+    });
+
+    $('#menu').on('mouseleave', function(){
+      $('#menu li:not(:first)').removeClass('showMenuItem');
+    });
+
+    // $('body').not('nav').on('click', function(){
+    //   $('#menu li:not(:first)').removeClass('showMenuItem');
+    // });
     // console.log(s.node().getBBox());
     // .each(function(d, i){
     //   console.log(d, i, this);//, this.getBBox());
